2bc06f60cbee5782492c197d383845bb51569542
katas8n <kirka6061512@icloud.com>
2023-02-16 21:06:35 +0200

    New task

--- lesson-1/practice/guess-the-number.py
+++ lesson-1/practice/guess-the-number.py
@@ -5,3 +5,33 @@
 # 5) if user_number equal to our , say to user : Impossible
 # 5) if user_number is not a number : WTH ?
 # 6) Progam works until user guess the number
+
+
+# magic_number = 23
+
+# switcher = True
+
+# while switcher:
+#     user_number = input("Eneter your number : ")
+
+#     user_number_number_typed = int(user_number)
+
+#     if magic_number == user_number_number_typed:
+#         print("Impassible")
+#         switcher = False
+#     # * elif (if else)
+#     elif magic_number < user_number_number_typed:
+#         print("Ur number bigger than mine")
+#     elif magic_number > user_number_number_typed:
+#         print("Ur number less than mine")
+
+
+# * ++++++++++++++++++++++++++++++++++++++++++++++
+
+# 1) get user name , if user_name length < 2 => Incorrect data , try again
+# 2) user_name length > 2 => Do you want to register here ? y/n :
+# 3) if "y" -> enter your email => if email contain "@" -> it's valid , otherwise -> invalid -> try again
+
+# 4) if "n" -> goodbye
+# 5) if something else -> incorrect data try again later
+# 6) Programm works until user enter correct (valid) data ;
