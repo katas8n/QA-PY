acd252d849c8eb558551250ae10589ec9ca7f9ae
katas8n <kirka6061512@icloud.com>
2023-02-17 21:32:04 +0200

    Added something new

--- lesson-2/practice/practice.py
+++ lesson-2/practice/practice.py
@@ -4,7 +4,13 @@
 # Karolina
 # Kirolini
 
+# user_name = input("Enter ur name : ")
+# print(user_name.replace("a", "i"))
+
 # 2) Get favorit lyric row from song from user and split it
+# lyrics = input("Eneter the row")
+
+# print(lyrics.split(" "))
 
 # EXAMPLE
 # 'Cause we're choking
@@ -18,8 +24,14 @@
 # EXAMPLE
 # kiR
 # KIR
+# user_name = "kir"
+# print(user_name.upper())
 
 # 4) Get user name from upperCase convert it to lowerCase and than capitalize it
+
+# user_name = "KiR"
+
+# print(user_name.lower().capitalize())
 
 # EXAMPLE
 # KIR
@@ -27,6 +39,30 @@
 # Kir
 
 # 5) Get from user expression that contain 20 expressions , but then - cut it to 20 symbols
+# expression = """
+# Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, odio itaque
+# mollitia hic enim ipsam a sed officiis molestias minus perferendis suscipit
+# beatae amet rerum, qui fugiat adipisci, quis aperiam excepturi fugit blanditiis.
+# Molestias non velit laudantium tenetur, iure sit quidem earum repellat enim
+# accusantium quasi. Doloribus adipisci id, atque fugit nobis eligendi nulla
+# voluptatibus earum asperiores dolores totam sint? Alias et exercitationem
+# perspiciatis quisquam sunt, iure id commodi vero nesciunt harum voluptatibus,
+# quia recusandae tenetur vitae ipsa iste modi corporis qui officia molestiae
+# explicabo voluptas maxime necessitatibus aut? Debitis minus molestiae in labore
+# id, nulla, aliquam porro fuga esse ex doloremque cumque quos repudiandae ratione
+# ipsa. Modi earum, amet distinctio perspiciatis ea aperiam dignissimos quidem sed
+# nulla autem quaerat soluta eum nihil iure ullam dolorem iste adipisci quasi
+# asperiores? Magni dolorum atque, consequatur delectus ullam minus incidunt omnis
+# culpa molestiae numquam sequi a consequuntur est, molestias accusantium
+# repellendus accusamus quas dicta eveniet fugiat, natus pariatur deserunt
+# excepturi. Eum, suscipit illo. Officia ipsam in aspernatur sit fuga. Laborum
+# nihil enim consequatur molestias eius, labore unde! Illo consequatur officia
+# doloribus quod dolore eum et ullam rerum facilis voluptatum voluptates, eaque
+# veritatis est, quia quasi asperiores perferendis aliquam, ducimus aut laborum?
+# Ipsam, eius culpa. Rerum, corrupti fuga?
+# """
+
+# print(expression[:20])
 
 # EXAMPLE
 #  "loremofsm dfk mlskdmf lskdmlsdk mfsldkf mlsdf lskdm lksdm lfkm lskfdm lsdkm flskf msldf lskdng lksndglk n lskdn lknsd lkn ldsknglk snglksdn lgkn lkndlgk nlsdkng lkns"
---
+++ lesson-2/structurs/lists-tuples/list.py
@@ -0,0 +1,38 @@
+# group = [
+#     "John",
+#     "Mike",
+#     "Bob",
+#     "Joe"
+# ]
+
+# john = group[0]
+
+# print(group)
+# print(" !!! ".join(group))
+# print(john)
+
+# ! lists
+
+# group = list("Mike")
+# group = list()
+
+# method
+# group.insert(0, "Mike")
+# DELETE , PUSH , DROP , EDIT
+# group = [
+#     "Mike",
+#     "John",
+#     "Bob",
+#     "Joe"
+# ]
+
+# print(group)
+
+# fibanachi_nums = [1, 2, 3, 5, 8, 13, 21]
+
+# Tuples
+# data = ("John", "Mike", 23, False)
+
+# print(data)
+# print(data[0])
+# print(data[-1])
