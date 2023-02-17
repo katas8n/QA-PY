dc396ab3ff3b6823c3f8d8366743fb1041d9c78c
Yurko35 <124586450+Yurko35@users.noreply.github.com>
2023-02-17 16:58:37 +0200

    Create YURA-HW.py

---
+++ hw/YURA-HW.py
@@ -0,0 +1,14 @@
+user_name = input ()
+print ("What is your name?")
+user_name = input ()
+print ("Hello,", user_name)
+
+mail_adress = input ()
+print ("What is your e-mail?")
+mail_adress = input ()
+print ("Your e-mail",mail_adress)
+
+birthday_user = input ()
+print ("What is your year of birth?")
+birthday_user = input ()
+print ("Your birthday",birthday_user)
